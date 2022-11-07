4d921c5119972d6ddadbb796a2658e2c918a84cc
Fbouchezpro <fbouchezpro@gmail.com>
2022-11-07 17:04:29 +0100

    Ending quest 5

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.post("/api/users", userHandlers.postUser);
 
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
